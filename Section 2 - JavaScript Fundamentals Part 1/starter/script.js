8e35159e871abf406b6e74470ef972c834fcdd34
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-11-18 14:19:04 +0100

    Starting a new course

---
+++ Section 2 - JavaScript Fundamentals Part 1/starter/script.js
@@ -0,0 +1,16 @@
+
+// Variables
+
+let firstName = `Ilija`;
+console.log(firstName);
+
+// Assigment
+
+let country = `Macedonia`;
+let continent = `Europe`;
+let population = 2;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
