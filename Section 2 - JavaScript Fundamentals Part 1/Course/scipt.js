80dd710b262d92697a06496066d747b20884d06c
ilijaaerodrom <ilija_trajkovski@outlook.com>
2023-01-03 22:09:06 +0100

    continue with the course

--- Section 2 - JavaScript Fundamentals Part 1/Course/scipt.js
+++ Section 2 - JavaScript Fundamentals Part 1/Course/scipt.js
@@ -1,2 +1,25 @@
 
 
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageSarah / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = `Ilija`;
+const lastName = `Trajkovski`;
+console.log(firstName + ` ` + lastName);
+
+
+// Assigment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x++; // x = x + 1
+console.log(x);
+
+
+// Comperison oprators
+// do 12.38
