7560f58cdb75419035c9a156fffff8255151e81a
ilijaaerodrom <ilija_trajkovski@outlook.com>
2023-01-09 14:34:47 +0100

    almost to challenge 3

--- Section 2 - JavaScript Fundamentals Part 1/Course/scipt.js
+++ Section 2 - JavaScript Fundamentals Part 1/Course/scipt.js
@@ -22,4 +22,61 @@ console.log(x);
 
 
 // Comperison oprators
-// do 12.38
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+// Strings and Template Literals
+const myName = `Ilija`;
+const job = `counsel`;
+const birthYear = 1986;
+const year = 2023;
+
+const ilija = `I'm ` + myName + `, a ` + (year - birthYear) +
+` years old ` + job + `!`;
+console.log(ilija);
+
+const ilijaNew = `I'm ${myName},a ${year - birthYear} 
+years old ${job}!`;
+console.log(ilijaNew);
+
+console.log(`String
+multi
+lines`);
+
+// if / else Statemnets
+const age = 15;
+
+if (age >= 18) {
+  console.log(`Sarah can start driving licence`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is to young.Wait another
+  ${yearsLeft} years`);
+ };
+
+
+const yearOfBirth = 1986;
+let century;
+
+if (yearOfBirth <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+};
+console.log(century);
+
+// 5 falsy values: 0, ``, undefined, null, Nan
+const money = 0; // falsy - boolean means false
+if (money) {
+  console.log(`Don't spend it all`);
+} else {
+  console.log(`Get a job`);
+};
+
+// Boolean logic
